c0be76ae7f2fce275a2c1b0c686d1b386d988447
kriwsha <havenoclue13>
2018-03-15 16:01:04 +0300

    sentence correction

--- src/main/java/bva/json/App.java
+++ src/main/java/bva/json/App.java
@@ -1,12 +1,16 @@
 package bva.json;
 
+import bva.json.types.TextRandom;
+
 public class App {
 
     public static void main(String[] args) {
-
+        generate();
     }
 
     private static void generate() {
-
+        TextRandom text = new TextRandom();
+        for (int i =0 ; i<10; i++)
+            System.out.println(text.next());
     }
 }
