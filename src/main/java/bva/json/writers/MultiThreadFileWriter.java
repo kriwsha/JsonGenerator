de033546c395f929d0476d7429fbdbfe33daba0d
kriwsha <semon-ka@mail.ru>
2018-03-16 22:53:09 +0300

    writer

--- src/main/java/bva/json/writers/MultiThreadFileWriter.java
+++ src/main/java/bva/json/writers/MultiThreadFileWriter.java
@@ -1,7 +1,12 @@
 package bva.json.writers;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MultiThreadFileWriter {
     private static volatile MultiThreadFileWriter instance;
+    private File file;
 
     public static MultiThreadFileWriter getInstance() {
         MultiThreadFileWriter local = instance;
@@ -17,9 +22,22 @@ public class MultiThreadFileWriter {
     }
 
     private MultiThreadFileWriter() {
+        try {
+            file = new File("result.txt");
+            if (!file.exists())
+                file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("error: " + e.getMessage());
+        }
     }
 
-    public synchronized void writeToFile() {
-
+    public synchronized void writeToFile(String json) {
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(json + "\r");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
     }
 }
