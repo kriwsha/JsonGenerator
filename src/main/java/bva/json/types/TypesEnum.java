4a3484c0a536cce76776dd6012b14e286972f82c
kriwsha <havenoclue13>
2018-03-16 14:32:33 +0300

    generators

--- src/main/java/bva/json/types/TypesEnum.java
+++ src/main/java/bva/json/types/TypesEnum.java
@@ -1,91 +1,91 @@
 package bva.json.types;
 
 public enum TypesEnum {
-    RANDOMSTR {
+    STR {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.StringRandom");
         }
     },
-    RANDOMTEXT {
+    TEXT {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.TextRandom");
         }
     },
     NUMBER {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.NumberRandom");
         }
     },
     DATETIME {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.DateTimeRandom");
         }
     },
     DATE {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.DateRandom");
         }
     },
     TIME {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.TimeRandom");
         }
     },
     GUID {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.GuidRandom");
         }
     },
     BOOL {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.BoolRandom");
         }
     },
     PHONE {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.PhoneRandom");
         }
     },
     NAME {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.NameRandom");
         }
     },
     EMAIL {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.EmailRandom");
         }
     },
     CURRENCY {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.CurrencyRandom");
         }
     },
     WEBSITE {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.WebSiteRandom");
         }
     },
     GENDER {
         @Override
-        public Class<?> getRandomClass() throws Exception {
+        public Class<?> getClassRandom() throws Exception {
             return Class.forName("bva.json.types.GenderRandom");
         }
     };
 
-    public abstract Class<?> getRandomClass() throws Exception;
+    public abstract Class<?> getClassRandom() throws Exception;
 
 }
