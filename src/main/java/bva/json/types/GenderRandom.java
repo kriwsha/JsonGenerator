22596e0bd1dbeda2e5e3298d6ec1d0a20fdd3168
kriwsha <havenoclue13>
2018-03-21 08:01:15 +0300

    generators

--- src/main/java/bva/json/types/GenderRandom.java
+++ src/main/java/bva/json/types/GenderRandom.java
@@ -3,10 +3,20 @@ package bva.json.types;
 import java.util.Random;
 
 public class GenderRandom implements RandomStrObj{
+    private String man;
+    private String woman;
 
+    public GenderRandom() {
+        this("m", "w");
+    }
+
+    public GenderRandom(String man, String woman) {
+        this.man = man;
+        this.woman = woman;
+    }
 
     @Override
     public String next() {
-        return new Random().nextBoolean()? "m" : "w";
+        return new Random().nextBoolean()? man : woman;
     }
 }
