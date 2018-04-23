5db4e4187e4b93861611c9106aa13d9cb480dde3
kriwsha <semon-ka@mail.ru>
2018-04-23 20:18:30 +0300

    phone random

--- src/main/java/bva/json/types/PhoneRandom.java
+++ src/main/java/bva/json/types/PhoneRandom.java
@@ -1,18 +1,16 @@
 package bva.json.types;
 
-import java.util.Random;
-
-public class PhoneRandom extends Randomizer {
+public class PhoneRandom extends Randomizer implements RandomStrObj {
     private final char[] chars = "1234567890".toCharArray();
 
     public String next() {
         StringBuilder phone = new StringBuilder();
-        phone.append("+").append(chars[random.nextInt(chars.length-1)]).append("(");
-        for (int i=1; i<=3; i++)
-            phone.append(chars[random.nextInt(chars.length-1)]);
-        phone.append(")");
-        for (int i=1; i<=4; i++)
-            phone.append(chars[random.nextInt(chars.length-1)]);
+        phone.append("+").append(chars[random.nextInt(chars.length - 1)]).append(" (");
+        for (int i = 1; i <= 7; i++) {
+            if (i == 4)
+                phone.append(") ");
+            phone.append(chars[random.nextInt(chars.length - 1)]);
+        }
         return phone.toString();
     }
 }
